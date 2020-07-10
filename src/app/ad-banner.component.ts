9ccd5ffb7600c4b2680b264335eaa53098e8c8d1
otera <capacityoverflow@reminiscence.sakura.ne.jp>
2020-07-10 15:03:04 +0900

    Change to display repeatedly for AdItem

--- src/app/ad-banner.component.ts
+++ src/app/ad-banner.component.ts
@@ -1,51 +1,51 @@
-import { Component, Input, OnInit, ViewChild, ComponentFactoryResolver, OnDestroy } from '@angular/core';
+import {
+  Component,
+  Input,
+  AfterViewInit,
+  ViewChildren,
+  ComponentFactoryResolver,
+  QueryList,
+} from '@angular/core';
 
 import { AdDirective } from './ad.directive';
-import { AdItem }      from './ad-item';
+import { AdItem } from './ad-item';
 import { AdComponent } from './ad.component';
 
 @Component({
   selector: 'app-ad-banner',
   template: `
-              <div class="ad-banner-example">
-                <h3>Advertisements</h3>
-                <ng-template ad-host></ng-template>
-              </div>
-            `
+    <div class="ad-banner-example">
+      <h3>Advertisements</h3>
+      <ng-container *ngFor="let ad of ads">
+        <ng-template ad-host></ng-template>
+      </ng-container>
+    </div>
+  `,
 })
-export class AdBannerComponent implements OnInit, OnDestroy {
+export class AdBannerComponent implements AfterViewInit {
   @Input() ads: AdItem[];
   currentAdIndex = -1;
-  @ViewChild(AdDirective, {static: true}) adHost: AdDirective;
-  interval: any;
+  @ViewChildren(AdDirective) adHosts: QueryList<AdDirective>;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
 
-  ngOnInit() {
-    this.loadComponent();
-    this.getAds();
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.interval);
+  ngAfterViewInit(): void {
+    // How to ExpressionChangedAfterItHasBeenCheckedError
+    Promise.resolve(null).then(() => this.loadComponent());
   }
 
   loadComponent() {
-    this.currentAdIndex = (this.currentAdIndex + 1) % this.ads.length;
-    const adItem = this.ads[this.currentAdIndex];
+    for (let index = 0; index < this.ads.length; index++) {
+      const adItem = this.ads[index];
+      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(
+        adItem.component
+      );
 
-    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(adItem.component);
+      const viewContainerRef = this.adHosts.toArray()[index].viewContainerRef;
+      viewContainerRef.clear();
 
-    const viewContainerRef = this.adHost.viewContainerRef;
-    viewContainerRef.clear();
-
-    const componentRef = viewContainerRef.createComponent(componentFactory);
-    (<AdComponent>componentRef.instance).data = adItem.data;
-  }
-
-  getAds() {
-    this.interval = setInterval(() => {
-      this.loadComponent();
-    }, 3000);
+      const componentRef = viewContainerRef.createComponent(componentFactory);
+      (<AdComponent>componentRef.instance).data = adItem.data;
+    }
   }
 }
